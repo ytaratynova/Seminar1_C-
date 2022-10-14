9a27aa8d4f961752f23de385933e938ad3b9ccac
Iuliia <ytaratynova@gmail.com>
2022-10-14 15:30:35 +0300

    Task8

---
+++ Task8/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.Clear();
+
+Console.Write("Введите натуральное число: ");
+int number = int.Parse(Console.ReadLine());
+
+int count = 2;
+
+if (number > 1)
+{
+    while (count < (number / 2)*2 + 1)
+    {
+        Console.Write($"{count}, ");
+        count = count + 2;
+    }
+}
+else
+{
+    Console.Write("Нет данных, удовлетворящих условию задачи");
+}
+
